23fdb970b25229da5a8aa22b8c3b427580a3e76a
Peter Meerwald-Stadler <pmeerw@pmeerw.net>
2018-06-16 14:03:12 +0200

    service: Fix wrong use of wrong enum type in __connman_service_reset_ipconfig()
    address_updated() takes enum connman_ipconfig_type, not
    enum connman_ipconfig_method.
    
    CID 1393449

--- src/service.c
+++ src/service.c
@@ -3414,7 +3414,7 @@ int __connman_service_reset_ipconfig(struct connman_service *service,
 			*new_state = service->state_ipv6;
 
 		settings_changed(service, new_ipconfig);
-		address_updated(service, new_method);
+		address_updated(service, type);
 
 		__connman_service_auto_connect(CONNMAN_SERVICE_CONNECT_REASON_AUTO);
 	}
