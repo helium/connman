4bc39473713a19e70e94c620638c33571abf81ba
Slava Monich <slava.monich@jolla.com>
2018-08-16 17:03:31 +0300

    inet: Invoke rtnl callback on errors too
    Not invoking the callback results in memory leaks like this one:
    
    ==6817== 30 (8 direct, 22 indirect) bytes in 1 blocks are definitely lost in loss record 1,600 of 3,200
    ==6817==    at 0x483F3EC: malloc (vg_replace_malloc.c)
    ==6817==    by 0x8DC1B: __connman_inet_get_route (inet.c)
    ==6817==    by 0x5E2E7: set_vpn_routes (connection.c)
    ==6817==    by 0x5FD77: __connman_connection_gateway_add (connection.c)
    ==6817==    by 0x837F7: __connman_ipconfig_gateway_add (ipconfig.c)
    ==6817==    by 0x7DFA7: set_connected (provider.c)
    ==6817==    by 0x7E0CB: connman_provider_set_state (provider.c)
    ==6817==    by 0x499C3: set_provider_state (vpn.c)
    ==6817==    by 0x4D95F: property_changed (vpn.c)
    ==6817==    by 0xD7D4B: signal_filter (watch.c)
    ==6817==    by 0xD8323: message_filter (watch.c)
    ==6817==    by 0x4A1E02F: dbus_connection_dispatch (dbus-connection.c)

--- src/inet.c
+++ src/inet.c
@@ -2370,6 +2370,7 @@ static gboolean inet_rtnl_event(GIOChannel *chan, GIOCondition cond,
 		return TRUE;
 
 cleanup:
+	rtnl_data->callback(NULL, rtnl_data->user_data);
 	inet_rtnl_cleanup(rtnl_data);
 	return TRUE;
 }
