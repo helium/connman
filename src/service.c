acf200507c9a9648f0839d9a11d0a1215ae99a41
Benoît Monin <benoit.monin@gmx.fr>
2018-10-14 18:33:08 +0200

    service: Signal when services are moved
    When calling MoveBefore or MoveAfter on a service, no signal is emitted
    by ConnMan. This makes it impossible for a D-Bus client to maintain an
    ordered list of services in sync with ConnMan.
    
    This patch schedules the emission of the signal ServicesChanged from the
    function move_service(), so the new list will get published over D-Bus.

--- src/service.c
+++ src/service.c
@@ -4681,6 +4681,8 @@ static DBusMessage *move_service(DBusConnection *conn,
 
 	__connman_connection_update_gateway();
 
+	service_schedule_changed();
+
 	return g_dbus_create_reply(msg, DBUS_TYPE_INVALID);
 }
 
